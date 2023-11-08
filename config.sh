90b96f68cc68366f24ec627024b7e8bca09eb886
dmtrippyhippy <73546521+dmtrippyhippy@users.noreply.github.com>
2023-11-08 14:06:35 -0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,8 +2,8 @@
 
 # about the custom recovery
 export NAME="twrp" # supported: pbrp, shrp, twrp
-export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp" # the link of manifest
-export BRANCH="twrp-12.1" # the branch of manifest
+export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni" # the link of manifest
+export BRANCH="twrp-9.0" # the branch of manifest
 
 # about your device
 export DEVICE="tonga" # codename used in device tree
@@ -11,7 +11,7 @@ export DT_LINK="https://github.com/twrpdtgen/android_device_motorola_tonga" # de
 export DT_BRANCH="tonga_g-user-12-S3RQS32.20-42-10-12-5-1-df86f-release-keys" # device tree branch
 export VENDOR="motorola" # device manufacturer or vendor
 export TARGET="bootimage" # build method: recoveryimage, bootimage, vendorboot
-export TYPE="aosp" # recovery type: omni, twrp, something else
+export TYPE="omni" # recovery type: omni, twrp, something else
 
 # you dont need to edit these
 export SYM="_"
